13d55dfe1aff0af84a088cbd453e51b196242e9b
Peter Clark <peterguisadio.ctc@gmail.com>
2024-05-08 01:31:52 +0800

    Reworked Nav Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,18 @@
-import { FC } from "react"
+import { FC, MouseEventHandler, createContext, useContext, useState } from "react"
 import { NavLink } from "react-router-dom"
 import ColorGroup, { ColorSelect } from "../utils/ColorGroup"
+
+type SidebarContextObj = {
+    open: () => void,
+    close: () => void,
+    isOpen: boolean
+}
+const initSidebarContext: SidebarContextObj = {
+    open: () => { },
+    close: () => { },
+    isOpen: true
+}
+const SidebarContext = createContext(initSidebarContext);
 
 export type NavLinkItem = {
     to: string,
@@ -12,24 +24,38 @@ type SidebarSectionProps = {
     navlinks: Array<NavLinkItem>
 }
 const SidebarSection: FC<SidebarSectionProps> = ({ title, navlinks }) => {
+    const [showLinks, setShowLinks] = useState(true);
+    const sidebarcontext = useContext(SidebarContext);
+
+    const navClickHandler = () => {
+        sidebarcontext.close();
+    }
+
     const mapToLink = (navItem: NavLinkItem, index: number) => {
         return (
             <li key={`nav_${title}_${index}_${navItem.text}`} >
-                <NavLink className="font-bold" to={navItem.to}>
+                <NavLink className="font-bold" to={navItem.to} onClick={navClickHandler}>
                     <div className="w-full p-3 hover:bg-gray-light dark:hover:bg-gray rounded-md">{navItem.text}</div>
                 </NavLink>
             </li>
         )
     }
 
+    const handleTitleClick: MouseEventHandler = () => {
+        setShowLinks((showLinks) => !showLinks);
+    }
+
+    const chevronClass = showLinks ? "bi bi-chevron-down" : "bi bi-chevron-right";
+
     return (
         <>
-            <div className="text-lg font-bold mb-2">
-                {title}
-            </div>
-            <ul className="ml-2">
+            {title && (<div className="text-lg font-bold p-2 hover:cursor-pointer rounded-md" onClick={handleTitleClick}>
+                <span>{title} &nbsp; <i className={chevronClass}></i></span>
+            </div>)}
+            {showLinks && (<ul className="ml-2">
                 {navlinks.map(mapToLink)}
-            </ul>
+            </ul>)}
+            <hr className="my-2 text-opacity-70 dark:text-opacity-70" />
         </>
     )
 }
@@ -43,16 +69,37 @@ const SidebarMain: FC<SidebarProps> = (props) => {
     const { children, title, colorGroup = ColorGroup.inherit } = props;
     const colorClasses = ColorSelect[colorGroup];
 
+    const [showLinks, setShowLinks] = useState((window.innerWidth >= 1024));
+
+    const sidebarcontext: SidebarContextObj = {
+        close: () => { setShowLinks(false) },
+        open: () => { setShowLinks(true) },
+        isOpen: showLinks
+    }
+
+    const clickHamburgerHandler = () => {
+        setShowLinks((showLinks) => !showLinks);
+    }
+
+    const linksClassNames = !showLinks ? "hidden lg:block" : "";
+
     const renderTitle = title && (
-        <div className={`text-lg text-opacity-70 dark:text-opacity-70 font-bold mb-5 ${colorClasses.titleColorClass}`}>
+        <div className={`text-lg text-opacity-70 dark:text-opacity-70 font-bold ${colorClasses.titleColorClass}`}>
             {title}
+            <span className="float-right lg:hidden"><button type="button" onClick={clickHamburgerHandler}><i className="bi bi-list"></i></button></span>
         </div>
     )
+
     return (
-        <div className={`min-w-52 max-w-96 h-full px-10 py-5 bg-opacity-70 dark:bg-opacity-80 ${colorClasses.textColorClass} ${colorClasses.bgColorClass}`}>
-            {renderTitle}
-            {children}
-        </div>
+        <SidebarContext.Provider value={sidebarcontext}>
+            <div className={`min-w-52 h-full px-10 py-5 ${colorClasses.textColorClass} ${colorClasses.bgColorClass}`}>
+                {renderTitle}
+                <div className={linksClassNames}>
+                    <hr className="my-2" />
+                    {children}
+                </div>
+            </div>
+        </SidebarContext.Provider>
     )
 }
 
--- src/pages/crudpages/CrudLayoutPage.tsx
+++ src/pages/crudpages/CrudLayoutPage.tsx
@@ -8,11 +8,17 @@ export default function CrudLayoutPage() {
         { to: "userroles", text: "User Roles" }
     ];
 
+    const navLinks2: Array<NavLinkItem> = [
+        { to: "test1", text: "Test1" },
+        { to: "test2", text: "Test 2" }
+    ];
+
     return (
-        <div className="flex flex-1">
+        <div className="flex flex-col lg:flex-row flex-1">
             <div className="flex-none">
                 <Sidebar.Main title="CRUD Samples" colorGroup={ColorGroup.accent}>
-                    <Sidebar.Section title="User Maintenance" navlinks={navLinks} />
+                    <Sidebar.Section key={"sidebar_section0"} title="User Maintenance" navlinks={navLinks} />
+                    <Sidebar.Section key={"sidebar_section1"} navlinks={navLinks2} />
                 </Sidebar.Main>
             </div>
             <div className="flex-1">
