19438a15be8850b2ea9350c76931ce3f81e4a36b
Peter Clark <peterguisadio.ctc@gmail.com>
2024-05-15 15:40:18 +0800

    Hide pagination btns if theres only 1 page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -28,29 +28,31 @@ const Pagination: FC<PaginationType> = ({ totalSize, pageSize = 10, onPaginate }
                 <span>{`Showing ${startIndex + 1}-${endIndex + 1} out of ${totalSize} rows`}</span>
             </div>
             <div className="flex-1">
-                <div className="sm:float-right">
-                    <Button type={currentPage !== 1 ? ButtonType.default : ButtonType.disabled}
-                        onClick={() => changePage(1)}>
-                        <i className="bi bi-chevron-bar-left"></i>
-                    </Button>
-                    &nbsp;
-                    <Button type={currentPage > 1 ? ButtonType.default : ButtonType.disabled}
-                        onClick={() => changePage(currentPage - 1)}>
-                        <i className="bi bi-chevron-left"></i>
-                    </Button>
-                    <span className="mx-5 font-bold">
-                        {currentPage} / {maxPage}
-                    </span>
-                    <Button type={currentPage < maxPage ? ButtonType.default : ButtonType.disabled}
-                        onClick={() => changePage(currentPage + 1)}>
-                        <i className="bi bi-chevron-right"></i>
-                    </Button>
-                    &nbsp;
-                    <Button type={currentPage !== maxPage ? ButtonType.default : ButtonType.disabled}
-                        onClick={() => changePage(maxPage)}>
-                        <i className="bi bi-chevron-bar-right"></i>
-                    </Button>
-                </div>
+                {maxPage > 1 && (
+                    <div className="sm:float-right">
+                        <Button type={currentPage !== 1 ? ButtonType.default : ButtonType.disabled}
+                            onClick={() => changePage(1)}>
+                            <i className="bi bi-chevron-bar-left"></i>
+                        </Button>
+                        &nbsp;
+                        <Button type={currentPage > 1 ? ButtonType.default : ButtonType.disabled}
+                            onClick={() => changePage(currentPage - 1)}>
+                            <i className="bi bi-chevron-left"></i>
+                        </Button>
+                        <span className="mx-5 font-bold">
+                            {currentPage} / {maxPage}
+                        </span>
+                        <Button type={currentPage < maxPage ? ButtonType.default : ButtonType.disabled}
+                            onClick={() => changePage(currentPage + 1)}>
+                            <i className="bi bi-chevron-right"></i>
+                        </Button>
+                        &nbsp;
+                        <Button type={currentPage !== maxPage ? ButtonType.default : ButtonType.disabled}
+                            onClick={() => changePage(maxPage)}>
+                            <i className="bi bi-chevron-bar-right"></i>
+                        </Button>
+                    </div>
+                )}
             </div>
         </div>
     )
